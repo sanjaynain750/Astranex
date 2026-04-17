57a2f1bb1b69a27220eff84968614309426d200a
Sanjay Nain <sanjaynain750@duck.com>
2026-04-17 21:52:45 +0530

    Create sploiter.py with exploit function
    Added a new sploiter module with an exploit function.

---
+++ modules/sploiter.py
@@ -0,0 +1,9 @@
+from utils.logger import Logger
+
+log = Logger("Sploiter")
+
+def exploit(target):
+    log.warning("Auto exploitation is disabled by default for safety.")
+    log.info("You can manually run exploits using Metasploit or similar tools.")
+    # यहाँ हम कुछ नहीं करेंगे, लेकिन फ्रेमवर्क तैयार है
+    pass
